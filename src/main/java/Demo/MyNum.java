5de840e95be78b9c71ac17438b303b7b36c353cd
wangh <wangh@emnets.org>
2023-08-14 11:15:54 +0800

    1

--- src/main/java/Demo/MyNum.java
+++ src/main/java/Demo/MyNum.java
@@ -1,15 +1,34 @@
 package Demo;
 
 public class MyNum {
-    int count;//记录个数
+    Long count;//记录个数
     double value;//先实现平均值的计算
 
     public MyNum() {
     }
 
-    public MyNum(int count, double value) {
+    public MyNum(Long count, double value) {
         this.count = count;
         this.value = value;
     }
 
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public void myprint(){
+        System.out.println("("+count+","+value+")");
+    }
 }
--- src/main/java/SourceL/DatagenSource.java
+++ src/main/java/SourceL/DatagenSource.java
@@ -1,31 +1,42 @@
 package SourceL;
 
+import Demo.MyNum;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.connector.source.util.ratelimit.RateLimiterStrategy;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.connector.datagen.source.DataGeneratorSource;
 import org.apache.flink.connector.datagen.source.GeneratorFunction;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 
 public class DatagenSource {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
         //env.setParallelism(2);
         //这个生成器在不同的并行算子中 10000会被拆分
-        DataGeneratorSource<String> dataGeneratorSource = new DataGeneratorSource<>(
-                new GeneratorFunction<Long, String>() {
+        DataGeneratorSource<MyNum> dataGeneratorSource = new DataGeneratorSource<>(
+                new GeneratorFunction<Long, MyNum>() {
                     @Override
-                    public String map(Long value) throws Exception {
-                        return "Number:" + String.valueOf(Math.random());
+                    public MyNum map(Long value) throws Exception {
+                        return new MyNum(1L,Math.random());
                     }
                 },
                 Long.MAX_VALUE,
                 RateLimiterStrategy.perSecond(10),
-                Types.STRING
+                Types.POJO(MyNum.class)
+                //POJO要求有getter 与 setter
         );
 
-        env.fromSource(dataGeneratorSource , WatermarkStrategy.noWatermarks(),"data-generator").print();
+        DataStreamSource<MyNum> ds = env.fromSource(dataGeneratorSource, WatermarkStrategy.noWatermarks(), "data-generator");
+        ds.addSink(new SinkFunction<MyNum>() {
+            @Override
+            public void invoke(MyNum value, Context context) throws Exception {
+
+                value.myprint();
+            }
+        });
 
         env.execute();
     }
